dd1d3050a7c851d5fdce878363dbfe9d1bc03362
Mathieu Marques <mmarques@dydu.ai>
2018-09-05 16:51:59 +0200

    Add <Bubble />

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import Bubble from '../Bubble';
+
 import './index.scss';
 
 
 class History extends React.Component {
   render() {
+    const bubbles = [];
     return (
       <div className="dydu-history">
-        History
+        {bubbles.map((it, index) => <Bubble children={it.text} key={index} type={it.type} />)}
       </div>
     );
   }
