73769364050c61b7d90de530c713acd23088796c
Mathieu Marques <mmarques@dydu.ai>
2019-08-28 19:02:04 +0200

    Add Configuration.initialize and .sanitize

--- src/tools/configuration.js
+++ src/tools/configuration.js
@@ -1,4 +1,6 @@
 import configuration from '../configuration';
+import axios from 'axios';
+import json from './configuration.json';
 import theme from '../styles/theme';
 
 
@@ -6,6 +8,10 @@ import theme from '../styles/theme';
  * Helper class to find values in a JSON configuration file.
  */
 export default class Configuration {
+
+  constructor() {
+    this.configuration = {};
+  }
 
   /**
    * Retrieve the value assiocated to the provided path or fallback when not
@@ -50,5 +56,53 @@ export default class Configuration {
       ...(top !== undefined && {top}),
       ...(width !== undefined && {width}),
     }))(data);
+  };
+
+  /**
+   * Fetch the configuration JSON and save its content.
+   *
+   * @param {string} [path] - Configuration file path.
+   * @returns {Promise}
+   */
+  initialize = (path='/configuration.json') => {
+    this.configuration = JSON.parse(JSON.stringify(json));
+    return axios.get(path).then(
+      ({ data }) => this.sanitize(data),
+      ({ request }) => {
+        // eslint-disable-next-line no-console
+        console.warn(`Configuration file not found at '${request.responseURL}'.`);
+        return this.configuration;
+      },
+    );
+  };
+
+  /**
+   * Clean up and ensure a sane structure for the configuration object.
+   *
+   * @param {Object} [data] - Configuration object to merge with.
+   * @returns {Object}
+   */
+  sanitize = data => {
+    const merge = (target, source) => {
+      if (target instanceof Object && source instanceof Object) {
+        Object.keys(source).forEach(key => {
+          const targetValue = target[key];
+          const sourceValue = source[key];
+          if (targetValue instanceof Object && sourceValue instanceof Object) {
+            target[key] = merge(Object.assign({}, targetValue), sourceValue);
+          }
+          else {
+            target[key] = sourceValue;
+          }
+        });
+      }
+      return target;
+    };
+    data = JSON.parse(JSON.stringify(data));
+    if (typeof data !== 'object') {
+      // eslint-disable-next-line no-console
+      console.warn('Invalid configuration file. Fallback to default values.');
+    }
+    return merge(this.configuration, data);
   };
 }
