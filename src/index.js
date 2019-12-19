53aa14a9b7cba9b9786b864b12efcad403d08abc
Mathieu Marques <mmarques@dydu.ai>
2019-12-19 12:14:36 +0100

    Minify class names

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'react-jss';
+import { JssProvider, ThemeProvider } from 'react-jss';
 import Application from './components/Application';
 import { ConfigurationProvider } from './contexts/ConfigurationContext';
 import { DialogProvider } from './contexts/DialogContext';
@@ -15,17 +15,19 @@ configuration.initialize().then(configuration => {
   const anchor = document.getElementById(configuration.root);
   if (anchor) {
     ReactDOM.render(
-      <ThemeProvider theme={theme}>
-        <ConfigurationProvider configuration={configuration}>
-          <DialogProvider>
-            <OnboardingProvider>
-              <ModalProvider>
-                <Application />
-              </ModalProvider>
-            </OnboardingProvider>
-          </DialogProvider>
-        </ConfigurationProvider>
-      </ThemeProvider>,
+      <JssProvider id={{minify: process.env.NODE_ENV === 'production'}}>
+        <ThemeProvider theme={theme}>
+          <ConfigurationProvider configuration={configuration}>
+            <DialogProvider>
+              <OnboardingProvider>
+                <ModalProvider>
+                  <Application />
+                </ModalProvider>
+              </OnboardingProvider>
+            </DialogProvider>
+          </ConfigurationProvider>
+        </ThemeProvider>
+      </JssProvider>,
       anchor,
     );
   }
