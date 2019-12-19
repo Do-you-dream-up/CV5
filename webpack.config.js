b85c1c76f5b6f0efc36cdcdf356fdb2c61a35826
Mathieu Marques <mmarques@dydu.ai>
2019-12-19 12:14:10 +0100

    Clean up

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 module.exports = environment => {
 
-  const isDevelopment = environment === 'development';
   const isProduction = environment === 'production';
 
   process.env.BABEL_ENV = environment;
@@ -16,45 +15,31 @@ module.exports = environment => {
     bail: true,
     devtool: 'source-map',
     entry: path.resolve(__dirname, 'src/index.js'),
-    mode: isProduction ? 'production' : isDevelopment && 'development',
+    mode: isProduction ? 'production' : 'development',
     module: {
       rules: [{
-        oneOf: [
-          {
-            include: path.resolve(__dirname, 'src/'),
-            loader: 'babel-loader',
-            options: {
-              cacheCompression: isProduction,
-              cacheDirectory: true,
-              compact: isProduction,
-              plugins: [
-                '@babel/plugin-proposal-class-properties',
-              ],
-              presets: [
-                '@babel/preset-env',
-                '@babel/preset-react',
-              ],
-              sourceMaps: false,
-            },
-            test: /\.js$/,
-          },
-          {
-            include: path.resolve(__dirname, 'src/'),
-            use: [
-              {loader: 'style-loader'},
-              {loader: 'css-loader'},
-              {loader: 'sass-loader'},
-            ],
-            test: /\.(sass|scss)$/,
-          },
-        ],
+        include: path.resolve(__dirname, 'src/'),
+        loader: 'babel-loader',
+        options: {
+          cacheCompression: isProduction,
+          cacheDirectory: true,
+          compact: isProduction,
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+          ],
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+          ],
+          sourceMaps: false,
+        },
+        test: /\.js$/,
       }],
       strictExportPresence: true,
     },
     output: {
       filename: 'bundle.min.js',
       path: path.resolve(__dirname, 'build/'),
-      pathinfo: isDevelopment,
     },
     plugins: [
       new CopyWebpackPlugin([path.resolve(__dirname, 'public/')], {ignore: ['index.html']}),
