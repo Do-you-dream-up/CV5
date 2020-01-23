eec6a4c2d5fcbea3d5e4b4a68fe0391030d2fca1
Mathieu Marques <mmarques@dydu.ai>
2020-01-23 17:31:46 +0100

    Prettify documentation

--- .styleguide.config.js
+++ .styleguide.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const docgen = require('react-docgen');
 const { version } = require('./package');
 const styles = require('./src/styleguide/styles');
+const theme = require('./src/styleguide/theme');
 
 
 module.exports = {
@@ -18,8 +19,10 @@ module.exports = {
   },
   styleguideComponents: {
     'PathlineRenderer': path.join(__dirname, 'src/styleguide/components/PathlineRenderer'),
+    'slots/IsolateButton': path.join(__dirname, 'src/styleguide/components/IsolateButton'),
   },
   styles,
+  theme,
   usageMode: 'expand',
   version,
 };
