4eeb1f53cbbf4f61769e91e29fe85edeee33491b
Mathieu Marques <mathieumarques78@gmail.com>
2020-02-03 16:22:21 +0100

    Fix Secondary width

--- src/components/Secondary/styles.js
+++ src/components/Secondary/styles.js
@@ -1,51 +1,10 @@
 import { createUseStyles } from 'react-jss';
 
 
-export default createUseStyles(theme => ({
-  actions: {
-    display: 'flex',
-    marginLeft: 'auto',
-    '& > :not(:first-child)': {
-      marginLeft: '1em',
-    },
-  },
-  frame: {
-    border: 0,
-    flexGrow: 1,
-  },
-  header: {
-    backgroundColor: `${theme.palette.background.secondary}CC`,
-    display: 'flex',
-    padding: '1.6em',
-    position: 'sticky',
-    top: 0,
-  },
-  over: {
-    boxShadow: 'none !important',
-    width: 'unset !important',
-  },
-  base: ({ configuration }) => ({
-    backgroundColor: theme.palette.background.secondary,
-    overflowY: 'auto',
-    ...configuration.secondary.styles,
-    [theme.breakpoints.down('xs')]: configuration.secondary.stylesMobile,
-  }),
-  body: {
-    padding: '1.6em',
-    paddingTop: 0,
-  },
-  left: {
-    extend: 'side',
-    right: '100%',
-    marginRight: '.5em',
-  },
-  right: {
-    extend: 'side',
-    left: '100%',
-    marginLeft: '.5em',
-  },
-  side: {
-    borderRadius: theme.shape.radius.inner,
+export default createUseStyles(theme => {
+
+  const side = {
+    borderRadius: theme.shape.radius.outer,
     bottom: 0,
     display: 'flex',
     flexDirection: 'column',
@@ -58,9 +17,54 @@ export default createUseStyles(theme => ({
       right: 0,
       width: 'unset !important',
     },
-  },
-  title: {
-    margin: 0,
-    marginRight: '1em',
-  },
-}));
+  };
+
+  return {
+    actions: () => ({
+      display: 'flex',
+      marginLeft: 'auto',
+      '& > :not(:first-child)': {
+        marginLeft: '1em',
+      },
+    }),
+    base: ({ configuration }) => ({
+      backgroundColor: theme.palette.background.secondary,
+      overflowY: 'auto',
+      ...configuration.secondary.styles,
+      [theme.breakpoints.down('xs')]: configuration.secondary.stylesMobile,
+    }),
+    body: () => ({
+      padding: '1.6em',
+      paddingTop: 0,
+    }),
+    frame: () => ({
+      border: 0,
+      flexGrow: 1,
+    }),
+    header: () => ({
+      backgroundColor: `${theme.palette.background.secondary}CC`,
+      display: 'flex',
+      padding: '1.6em',
+      position: 'sticky',
+      top: 0,
+    }),
+    left: () => ({
+      ...side,
+      right: '100%',
+      marginRight: '.5em',
+    }),
+    over: () => ({
+      boxShadow: 'none',
+      width: 'unset',
+    }),
+    right: () => ({
+      ...side,
+      left: '100%',
+      marginLeft: '.5em',
+    }),
+    title: () => ({
+      margin: 0,
+      marginRight: '1em',
+    }),
+  };
+});
