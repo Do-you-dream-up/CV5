0963fb559fabfeaa7543d15554b1bed059ad118f
Mathieu Marques <mathieumarques78@gmail.com>
2020-02-03 16:53:28 +0100

    Prettify Avatar

--- src/components/Avatar/styles.js
+++ src/components/Avatar/styles.js
@@ -2,23 +2,35 @@ import { createUseStyles } from 'react-jss';
 
 
 export default createUseStyles(theme => ({
+  background: ({ type }) => ({
+    alignItems: 'center',
+    backgroundColor: theme.palette[type].background,
+    display: 'flex',
+    justifyContent: 'center',
+    '& > *': {
+      maxHeight: '75%',
+      maxWidth: '75%',
+    },
+  }),
   base: ({ configuration }) => ({
     borderRadius: '50%',
     height: '3em',
     minWidth: '3em',
     width: '3em',
+    '& > *': {
+      height: '100%',
+      width: '100%',
+    },
     ...configuration.avatar.styles,
     [theme.breakpoints.down('xs')]: configuration.avatar.stylesMobile,
   }),
-  request: {
-    backgroundColor: theme.palette.request.background,
+  request: () => ({
     color: theme.palette.request.text,
-    marginLeft: '.5em !important',
+    marginLeft: '.5em',
     order: 2,
-  },
-  response: {
-    backgroundColor: theme.palette.response.background,
+  }),
+  response: () => ({
     color: theme.palette.response.text,
-    marginRight: '.5em !important',
-  },
+    marginRight: '.5em',
+  }),
 }));
