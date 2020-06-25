ddddc879e68fead381e9765b616380576180a7a2
Mathieu Marques <mathieumarques78@gmail.com>
2020-06-25 16:22:13 +0200

    Add usage sample of events for the teaser

--- src/components/Teaser/index.js
+++ src/components/Teaser/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ConfigurationContext } from '../../contexts/ConfigurationContext';
+import { EventsContext } from '../../contexts/EventsContext';
 import Skeleton from '../Skeleton';
 import useStyles from './styles';
 
@@ -13,13 +14,19 @@ import useStyles from './styles';
 export default function Teaser({ open, toggle }) {
 
   const { configuration } = useContext(ConfigurationContext);
+  const event = useContext(EventsContext).onEvent('teaser');
   const classes = useStyles({ configuration });
   const { ready, t } = useTranslation('teaser');
   const title = t('title');
 
+  const onClick = () => {
+    event('onClick', 1, 2, 4, 'asdf');
+    toggle(2)();
+  };
+
   return (
     <div className={c('dydu-teaser', classes.root, {[classes.hidden]: !open})}
-         onClick={toggle(2)}
+         onClick={onClick}
          title={title}>
       <div className={c('dydu-teaser-button', classes.button)}>
         <Skeleton children={title} hide={!ready} width="3em" />
