7753599ac6e3db9d7191b2d81382a3b57eff3cef
colinevel <coline.velard@gmail.com>
2020-10-07 12:31:37 +0200

    Configure contact tab for phone, email, social

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import ContactsList from '../ContactsList';
+import Paper from '../Paper/index';
 import useStyles from './styles';
 
 
 /**
- * Current dummy placeholder for a secondary tab.
+ * Contact tab
  */
 export default function Contacts() {
   const classes = useStyles();
-  return (
+  const { ready, t } = useTranslation('contacts');
+  const phoneTitle = t('phone.title');
+  const phones = t('phone.list');
+  const emailTitle = t('email.title');
+  const emails = t('email.list');
+  const socialTitle = t('socialNetwork.title');
+  const socialNetworks = t('socialNetwork.list');
+
+  return !!ready && (
     <div className={classes.root}>
-      <p>Contacts.</p>
+      <Paper elevation='4'>
+        <ContactsList id='phone' title={phoneTitle} list={phones} icon={'icons/phone.png'} />
+        <ContactsList id='email' title={emailTitle} list={emails} icon={'icons/email.png'} />
+        <ContactsList id='social' title={socialTitle} list={socialNetworks} icon={'icons/account.png'} />
+      </Paper>
     </div>
   );
 }
