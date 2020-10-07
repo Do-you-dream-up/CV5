831253b1065e05d658c263aab0212992c0373a34
colinevel <coline.velard@gmail.com>
2020-10-07 15:25:13 +0200

    Add wizard card for contacts

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ConfigurationContext } from  '../../contexts/ConfigurationContext';
 import ContactsList from '../ContactsList';
 import Paper from '../Paper/index';
 import useStyles from './styles';
@@ -9,6 +10,10 @@ import useStyles from './styles';
  * Contact tab
  */
 export default function Contacts() {
+  const { configuration } = useContext(ConfigurationContext);
+  const showPhone = configuration.contacts.phone;
+  const showEmail = configuration.contacts.email;
+  const showSocial = configuration.contacts.socialNetwork;
   const classes = useStyles();
   const { ready, t } = useTranslation('contacts');
   const phoneTitle = t('phone.title');
@@ -21,9 +26,9 @@ export default function Contacts() {
   return !!ready && (
     <div className={classes.root}>
       <Paper elevation={4}>
-        <ContactsList id='phone' title={phoneTitle} list={phones} icon={'icons/phone.png'} />
-        <ContactsList id='email' title={emailTitle} list={emails} icon={'icons/email.png'} />
-        <ContactsList id='social' title={socialTitle} list={socialNetworks} icon={'icons/account.png'} />
+        {showPhone && <ContactsList id='phone' title={phoneTitle} list={phones} icon={'icons/phone.png'} />}
+        {showEmail && <ContactsList id='email' title={emailTitle} list={emails} icon={'icons/email.png'} />}
+        {showSocial && <ContactsList id='social' title={socialTitle} list={socialNetworks} icon={'icons/account.png'} />}
       </Paper>
     </div>
   );
--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -7,41 +7,41 @@ import useStyles from './styles';
  * Contact list for phone numbers, emails and social networks
  */
 
-export default function ContactsList({icon, id, list, title}) {
-    const classes = useStyles();
+export default function ContactsList({ icon, id, list, title }) {
+  const classes = useStyles();
 
-    return (
-      <div className={classes.root}>
-        <div className={classes.title}>
-          <img alt={icon} src={icon}></img>
-          <h4>{title}</h4>
-        </div>
-        {list.map((item, index) => (
-          <div className={classes.list} key={index}>
-            <h5>{item.title}</h5>
-            {(id === 'phone') && <p>{item.phone}</p>}
-            {(id === 'email') && <a href={`mailto:${item.email}`}>{item.email}</a>}
-            {(id === 'social') &&
-              <div>
-                <a href={item.socialUrl} rel="noopener noreferrer" target='_blank'>{item.socialText}</a>
-                <img alt='icons/open-in-new.black.png' src='icons/open-in-new.black.png' />
-              </div>
-            }
-          </div>
-        ))}
+  return (
+    <div className={classes.root}>
+      <div className={classes.title}>
+        <img alt={icon} src={icon}></img>
+        <h4>{title}</h4>
       </div>
-    );
-  }
+      {list.map((item, index) => (
+        <div className={classes.list} key={index}>
+          <h5>{item.title}</h5>
+          {id === 'phone' && <p>{item.phone}</p>}
+          {id === 'email' && <a href={`mailto:${item.email}`}>{item.email}</a>}
+          {id === 'social' && (
+            <div>
+              <a href={item.socialUrl} rel="noopener noreferrer" target="_blank">{item.socialText}</a>
+              <img alt="icons/open-in-new.black.png" src="icons/open-in-new.black.png" />
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 ContactsList.propTypes = {
-    icon: PropTypes.string,
-    id: PropTypes.string,
-    list: PropTypes.arrayOf(PropTypes.shape({
-        email: PropTypes.string,
-        phone: PropTypes.string,
-        socialText: PropTypes.string,
-        socialUrl: PropTypes.string,
-        title: PropTypes.string.isRequired,
-      })).isRequired,
-    title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  id: PropTypes.string,
+  list: PropTypes.arrayOf(PropTypes.shape({
+      email: PropTypes.string,
+      phone: PropTypes.string,
+      socialText: PropTypes.string,
+      socialUrl: PropTypes.string,
+      title: PropTypes.string.isRequired,
+    })).isRequired,
+  title: PropTypes.string.isRequired,
 };
