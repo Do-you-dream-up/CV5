efa57e657f57a99e25b2577bc62107fc7edba234
Hamed Dhib <hameddhib@iMac-de-Hamed.local>
2020-10-30 16:22:50 +0100

    Fix weback cdn url error

--- webpack.production.js
+++ webpack.production.js
@@ -5,22 +5,21 @@ const webpack = require('webpack');
 const Merge = require('webpack-merge');
 const common = require('./webpack.common');
 
-
-module.exports = Merge.strategy({plugins: 'prepend'})(common, {
+module.exports = (env) =>  Merge.strategy({plugins: 'prepend'})(common, {
   devtool: 'source-map',
   mode: 'production',
   output: {
     filename: 'bundle.min.js',
     jsonpFunction: 'dydu.bliss',
-    path: Path.resolve(__dirname, 'build/v5'),
-    publicPath: './v5/'
+    path: Path.resolve(__dirname, 'build'),
+    publicPath: env.ASSET_PATH || './'
   },
   plugins: [
     new Clean(),
     new Copy([Path.resolve(__dirname, 'public/')], {ignore: ['index.html']}),
     new webpack.DefinePlugin({
       'process.env': {
-        PUBLIC_URL: JSON.stringify('./v5'),
+        PUBLIC_URL: JSON.stringify(env.ASSET_PATH || './'),
         }
     })
   ],
