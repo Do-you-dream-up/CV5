7a2152a5df2bd30c8432fcd6ba91d5325f5c89c6
Hamed Dhib <hameddhib@iMac-de-Hamed.local>
2020-11-09 12:03:27 +0100

    Fix img styles

--- src/components/Application/styles.js
+++ src/components/Application/styles.js
@@ -3,6 +3,11 @@ import { createUseStyles } from 'react-jss';
 
 export default createUseStyles(theme => ({
   root: () => ({
+    '& img' : {
+      display:'block',
+      height: 'auto',
+      maxWidth: '100%'
+    },
     fontFamily: theme.font.sansSerif,
   }),
 }));
