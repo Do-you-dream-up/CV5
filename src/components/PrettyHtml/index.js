cb161b74bddc6603348eb20c7eb56333f8acb547
colinevel <coline.velard@gmail.com>
2020-11-13 12:10:04 +0100

    Add email exception to PrettyHtml

--- src/components/PrettyHtml/index.js
+++ src/components/PrettyHtml/index.js
@@ -15,12 +15,17 @@ export default function PrettyHtml({ children, className, component, hasExternal
   const classes = useStyles();
   const { t } = useTranslation('translation');
 
+  const RE_EMAIL = /(.+)(<a href="mailto:\S+@\S+\.\S+").*(>.+<\/a>)(.+)/g;
+  const RE_ONCLICK = /onclick=".+"/gm;
+
+  const htmlCleanup = html && html.match(RE_EMAIL) ? html.replace(RE_ONCLICK, '') : html;
+
   const interactionType = type === 'response' ? t('screenReader.chatbot') : t('screenReader.me');
   return React.createElement(component, {className: c(classes.root, className), ...rest}, (
     <>
       {children}
       {<span className={classes.srOnly} dangerouslySetInnerHTML={{__html: interactionType}}></span>}
-      {html && <div dangerouslySetInnerHTML={{__html: html}} />}
+      <div dangerouslySetInnerHTML={{__html: htmlCleanup}} />
       {hasExternalLink && <img className={classes.externalLinkIcon} src={`${process.env.PUBLIC_URL}icons/open-in-new.black.png`} />}
     </>
   ));
