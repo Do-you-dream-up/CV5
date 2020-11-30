d104de0276d6b1eb1fffba4db18e60ce595ad372
colinevel <coline.velard@gmail.com>
2020-11-30 11:50:54 +0100

    Fix space between actions buttons

--- src/components/Actions/styles.js
+++ src/components/Actions/styles.js
@@ -4,7 +4,7 @@ import { createUseStyles } from 'react-jss';
 export default createUseStyles({
   root: {
     '& > :not(:last-child)': {
-      marginRight: '.1em',
+      marginRight: '.6em',
     },
     display: 'flex',
     justifyContent: 'flex-end',
