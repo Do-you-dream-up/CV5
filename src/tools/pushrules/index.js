e001b3e151b0549596c7d618515a8ca9b7ebcb1f
hdhib <hdhib@dydu.ai>
2021-01-08 08:49:15 +0100

    Fix parse palyload

--- src/tools/pushrules/index.js
+++ src/tools/pushrules/index.js
@@ -4,7 +4,7 @@ import { addRule, getExternalInfos, processRules } from './pushService';
 export default function fetchPushrules() {
     dydu.pushrules().then(data => {
         if (Object.keys(data).length > 0) {
-            const rules = JSON.parse('[' + data);
+            const rules = JSON.parse(data);
             rules.map(rule => {
                 addRule(rule);
             });
