cba4aa3f3be174aaf1d4c6517de17c465420c49e
hdhib <hdhib@dydu.ai>
2021-06-17 17:50:12 +0200

    Css fix overrdie

--- public/override/theme.js
+++ public/override/theme.js
@@ -1,5 +1,5 @@
 const css = JSON.parse(localStorage.getItem('dydu.css'));
 
 import theme from './theme.json';
-theme.palette.primary.main  = css && css.main ? css.main : '#3636B9';
+theme.palette.primary.main  = css && css.main ? css.main : theme.palette.primary.main;
 export default theme;
