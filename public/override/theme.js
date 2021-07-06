de12a9e0dc940e0de2ae5947e1cfa2c734790a5d
hdhib <hdhib@dydu.ai>
2021-07-06 17:09:59 +0200

    Fix css errors

--- public/override/theme.js
+++
@@ -1,5 +0,0 @@
-const css = JSON.parse(localStorage.getItem('dydu.css'));
-
-import theme from './theme.json';
-theme.palette.primary.main  = css && css.main ? css.main : theme.palette.primary.main;
-export default theme;
