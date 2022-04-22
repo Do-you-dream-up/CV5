85137f95527ce4d1d3ee73313386a5f7dc6db22c
Johanna Detrieux <jdetrieux@dydu.ai>
2022-04-22 14:35:59 +0000

    feat(main): #RDA-4433 Added responsive on mobile and tablet

--- public/chatboxHomepage.js
+++ public/chatboxHomepage.js
@@ -1,48 +1,96 @@
 window.DYDU_QUALIFICATION_MODE = true;
 
-const hideSelectedElements = () => {
-  // search all the span and input elements inside the first div and hide them
-  const containerVersion = document.getElementById("versionNumber");
-  const containerAddingUrl = document.getElementById('iframeForm');
-  containerVersion.style.display = "none";
-  containerAddingUrl.style.display = "none";
-}
-
-const submitIframe = () => {
-  if (!window.location.href.includes("?wizard") && document.getElementById("urlInput").value) {
-    const ifrm = document.getElementById("iframe");
-    ifrm.setAttribute("src", document.getElementById("urlInput").value);
-    ifrm.style.display = "block";
-    
-    // add iframeurl in url if "ok" button is clicked
-    const paramValue = document.getElementById("urlInput").value; // ex: https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal
-    const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set('iframeurl', paramValue);
-    window.history.replaceState({}, "", decodeURIComponent(`${window.location.pathname}?${urlParams}`)); // to push the new param in the url
-    
-    hideSelectedElements();
-  }
-};
-
-document.getElementById("button").addEventListener("click", submitIframe);
+// hide the homepage if the wizard cards are displayed
 if (window.location.href.includes("?wizard")) {
   document.getElementById("iframeForm").style.display="none";
 }
 
-if (window.location.href.includes("iframeurl=")) {
-  document.body.style.padding = "0"; // remove all margin to have the iframe on the whole page
+const hideSelectedElements = (status) => {
+  // search all the span and input elements inside the first div and hide them (except the button)
+  const containerVersion = document.getElementById("versionNumber");
+  const iframeFormElements = document.querySelectorAll('#iframeForm div');
+  const containerIframeForm = document.getElementById('iframeForm');
+  const actionToStopPreview = document.querySelector('#btnStopPreview');
   
-  hideSelectedElements();
-  
-  // retrieve the iframe element and give it dynamically the url passed in the query "iframeurl" to display the website behind the chatbox
-  const ifrm = document.getElementById("iframe");
-  const params = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-  });
-  // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-  let iframeurlValue = params.iframeurl; // "some_value"
-  
-  iframeurlValue = iframeurlValue.replace('https://', '');
-  ifrm.setAttribute("src", `https://${iframeurlValue}`);
-  ifrm.style.display = "block";
+  if (status) {
+    iframeFormElements.forEach((containerUrlElement) => {
+      containerUrlElement.style.display = "none";
+    });
+    containerIframeForm.style.height = 'initial';
+    containerVersion.style.display = "none";
+    actionToStopPreview.style.display = 'flex';
+  } else {
+    iframeFormElements.forEach((containerUrlElement) => {
+      containerUrlElement.style.display = "";
+    });
+    containerIframeForm.style.height = '80%';
+    containerVersion.style.display = "";
+    actionToStopPreview.style.display = 'none';
+  }
 }
+
+const updateIframe = (status, forcedUrl= null) => {
+  const ifrm = document.getElementById("iframe");
+  
+  if (forcedUrl !== null)  {
+    forcedUrl = forcedUrl.replace('https://', '');
+    ifrm.setAttribute("src", `https://${forcedUrl}`);
+    ifrm.style.display = "block";
+  } else {
+    if (status) {
+      let urlWebsite = document.getElementById("urlInput").value;
+      urlWebsite = urlWebsite.replace('https://', '');
+      ifrm.setAttribute("src", `https://${urlWebsite}`);
+      ifrm.style.display = "block";
+    } else {
+      ifrm.setAttribute("src", '');
+      ifrm.style.display = "none";
+    }
+  }
+  
+}
+
+const updateUrlParams = (status) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  
+  if (status) {
+    // add iframeurl in url if "Afficher" button is clicked
+    const paramValue = document.getElementById("urlInput").value; // ex: https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal
+    urlParams.set('iframeurl', paramValue);
+    window.history.replaceState({}, "", decodeURIComponent(`${window.location.pathname}?${urlParams}`)); // to push the new param in the url
+  } else {
+    window.history.replaceState({}, "", decodeURIComponent(`${window.location.pathname}`));
+  }
+}
+
+const submitIframe = (status) => {
+  if (
+    !window.location.href.includes("?wizard") &&
+    ((status && document.getElementById("urlInput").value) || status === false)
+  ) {
+    updateUrlParams(status);
+    updateIframe(status);
+    hideSelectedElements(status);
+  }
+};
+
+// retrieve the iframe element and give it dynamically the url passed in the query "iframeurl" to display the website behind the chatbox
+const addingTheLinkInIframeSource = () => {
+  if (window.location.href.includes("iframeurl=")) {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+      get: (searchParams, prop) => searchParams.get(prop),
+    });
+    
+    // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+    let iframeurlValue = params.iframeurl; // "some_value"
+  
+    hideSelectedElements(true);
+    updateIframe(null, iframeurlValue);
+  }
+}
+
+document.getElementById("btnDisplayIframe").addEventListener("click", () => submitIframe(true));
+
+document.getElementById('btnStopPreview').addEventListener('click', () => submitIframe(false));
+
+addingTheLinkInIframeSource();
