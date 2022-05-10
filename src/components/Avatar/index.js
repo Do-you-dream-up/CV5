b99659fbef0b9f9816fb9b087053fa5713be3d69
Maxime Gauché-Daumet <mgauchedaumet@dydu.ai>
2022-05-10 14:27:54 +0200

    fix(main): # Rollback for 4255

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,6 +1,6 @@
 import c from 'classnames';
 import PropTypes from 'prop-types';
-import React, { useContext, useMemo } from 'react';
+import React, { useContext } from 'react';
 import { ConfigurationContext } from '../../contexts/ConfigurationContext';
 import useStyles from './styles';
 const images = localStorage.getItem('dydu.images');
@@ -12,39 +12,31 @@ const images = localStorage.getItem('dydu.images');
  * A request avatar should be next to the user's input while a response avatar
  * should be next to its response.
  */
-const Avatar = ({ background, path, type, linkAvatarDependOnType }) => {
+export default function Avatar({ background, path, type }) {
   const { configuration } = useContext(ConfigurationContext);
   const classes = useStyles({ configuration, type });
   const logo = images && JSON.parse(images) && JSON.parse(images).logo;
   background = background !== undefined ? background : configuration.avatar.background;
 
-  const _path = useMemo(() => {
-    if (!linkAvatarDependOnType.includes('/null') && type !== 'request') {
-      return linkAvatarDependOnType;
-    } else if (path !== undefined) {
-      return path;
-    } else {
-      return {
-        request: `${process.env.PUBLIC_URL}assets/${configuration.avatar.request}`,
-        response: `${process.env.PUBLIC_URL}assets/${configuration.avatar.response}`,
-      }[type];
-    }
-  }, [configuration.avatar.request, configuration.avatar.response, linkAvatarDependOnType, path, type]);
-
-  const _className = useMemo(() => {
-    return c('dydu-avatar', `dydu-avatar-${type}`, classes.base, classes[type], {
-      [classes.background]: background,
-    });
-  }, [background, classes, type]);
-
-  return <div className={_className}>{!!_path && <img alt={`${type} avatar`} src={logo || _path} />}</div>;
-};
+  path =
+    path !== undefined
+      ? path
+      : {
+          request: `${process.env.PUBLIC_URL}assets/${configuration.avatar.request}`,
+          response: `${process.env.PUBLIC_URL}assets/${configuration.avatar.response}`,
+        }[type];
+  return (
+    <div
+      children={!!path && <img src={logo || path} />}
+      className={c('dydu-avatar', `dydu-avatar-${type}`, classes.base, classes[type], {
+        [classes.background]: background,
+      })}
+    />
+  );
+}
 
 Avatar.propTypes = {
   background: PropTypes.bool,
-  linkAvatarDependOnType: PropTypes.string,
   path: PropTypes.string,
   type: PropTypes.oneOf(['request', 'response']).isRequired,
 };
-
-export default Avatar;
