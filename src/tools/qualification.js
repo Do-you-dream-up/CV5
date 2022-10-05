7743132662bd3056803a98a8ee4cd3d5316f7b7c
Guillaume <gloup@dydu.ai>
2022-10-05 09:42:17 +0200

    fix(main): #RDA-5003 add test keyword qualification word

---
+++ src/tools/qualification.js
@@ -0,0 +1,5 @@
+export const QUALIFICATION = {
+  text: `
+    From now on, all your conversations will no longer be considered as a test and will be counted in the analytics, even after you refresh the page. To reactivate qualification mode, please close the tab.
+    `,
+};
