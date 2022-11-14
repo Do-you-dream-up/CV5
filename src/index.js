4409c52fb22fe0f5e80bf403021ae79a1c9b7e4f
Guillaume <gloup@dydu.ai>
2022-11-14 16:07:54 +0100

    fix(main): #RDS-3174 fix prefix css parent id

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import ViewModeProvider from './contexts/ViewModeProvider';
 import breakpoints from './styles/breakpoints';
 import { configuration } from './tools/configuration';
 import keycloak from './tools/keycloak';
-import { prefixCssSelectors } from './tools/css';
+import scope from 'scope-css';
 
 const css = JSON.parse(localStorage.getItem('dydu.css'));
 
@@ -65,7 +65,7 @@ configuration.initialize().then((configuration) => {
     Axios.get(`${process.env.PUBLIC_URL}override/style.css`).then((res) => {
       if (res?.data.length > 0) {
         const style = document.createElement('style');
-        style.textContent = prefixCssSelectors(res.data, `#${configuration.root}`);
+        style.textContent = scope(res.data, `#${configuration.root}`);
         document.head?.append(style);
       }
     });
