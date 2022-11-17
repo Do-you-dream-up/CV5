60b221ba26e7924377e9c289e30309f3507f0e5b
Guillaume <gloup@dydu.ai>
2022-11-17 14:57:01 +0100

    fix(main): #RDS-3348 fix detection space consultation

--- src/tools/dydu.js
+++ src/tools/dydu.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+
 import { Cookie, Local } from './storage';
 import { RESPONSE_QUERY_FORMAT, RESPONSE_TYPE, SOLUTION_TYPE } from './constants';
 import {
@@ -109,7 +110,7 @@ export default new (class Dydu {
   constructor() {
     this.onServerChangeFn = null;
     this.locale = this.getLocale();
-    this.space = this.getSpace();
+    this.space = this.getSpace(configuration?.spaces?.detection);
     this.emit = debounce(this.emit, 100, { leading: true });
     this.initInfos();
   }
