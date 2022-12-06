9879d3074027d6d4d71bccb81cd6d3bc320cd361
Maxime Gauché-Daumet <maxime.gd@gmail.com>
2022-12-06 16:10:25 +0100

    add variables

--- webpack.production.js
+++ webpack.production.js
@@ -15,8 +15,8 @@ module.exports = (env) => {
   const OIDC_CLIENT_ID = !QUALIFICATION ? JSON.stringify(configuration.oidc.clientIdProd) : JSON.stringify(configuration.oidc.clientIdPreprod);
   const OIDC_URL = !QUALIFICATION ? JSON.stringify(configuration.oidc.prodPorovider) : JSON.stringify(configuration.oidc.preprodPorovider);
   
-  if (env.CHATBOX_VERSION) {
-    ASSET = `${configuration.application.cdn}/${env.CHATBOX_VERSION}/`;
+  if (env.CHATBOX_VERSION && env.CHATBOX_VERSION !== '$CHATBOX_VERSION') {
+    ASSET = `${configuration.application.cdn}${env.CHATBOX_VERSION}/`;
     console.log(ASSET);
   }
   else {
