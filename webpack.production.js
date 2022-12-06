7bda3aff7dae8ebb6902d0e3385e0a94bd6ffba9
Maxime Gauché-Daumet <maxime.gd@gmail.com>
2022-12-06 12:28:37 +0100

    remove unused script

--- webpack.production.js
+++ webpack.production.js
@@ -1,67 +1,70 @@
 const Path = require('path');
-const { CleanWebpackPlugin: Clean  } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin: Clean } = require('clean-webpack-plugin');
 const Copy = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const Merge = require('webpack-merge');
 const configuration = require('./public/override/configuration.json');
 const common = require('./webpack.common');
-var PACKAGE = require('./package.json');
 
 module.exports = (env) => {
-
   let ASSET = './';
-  const QUALIFICATION = env.prod ? false : true;
-  const ONPREM = env.onprem ?  true : false;
-  const OIDC_CLIENT_ID = !QUALIFICATION ? JSON.stringify(configuration.oidc.clientIdProd) : JSON.stringify(configuration.oidc.clientIdPreprod);
-  const OIDC_URL = !QUALIFICATION ? JSON.stringify(configuration.oidc.prodPorovider) : JSON.stringify(configuration.oidc.preprodPorovider);
-  if (process.env.ASSET_FULL_URL) {
-    ASSET = process.env.ASSET_FULL_URL + '/';
-    console.log(ASSET);
-  }
-  else if (env[2]) {
-    ASSET = env[2] +  env.prod ? 'prod' : 'preprod' + '/';
-    console.log(ASSET);
-  }
-  else if (configuration.application.cdn && configuration.application.directory) {
-    ASSET =  configuration.application.cdn + configuration.application.directory;
-    if (!ONPREM ) {
-      ASSET += env.prod ? 'prod/' : 'preprod/';
-    }
-  }
 
-  return Merge.strategy({plugins: 'prepend'})(common(env), {
+  const QUALIFICATION = env.prod ? false : true;
+  // const ONPREM = env.onprem ?  true : false;
+  const OIDC_CLIENT_ID = !QUALIFICATION
+    ? JSON.stringify(configuration.oidc.clientIdProd)
+    : JSON.stringify(configuration.oidc.clientIdPreprod);
+  const OIDC_URL = !QUALIFICATION
+    ? JSON.stringify(configuration.oidc.prodPorovider)
+    : JSON.stringify(configuration.oidc.preprodPorovider);
+  // if (process.env.ASSET_FULL_URL) {
+  //   ASSET = process.env.ASSET_FULL_URL + '/';
+  //   console.log(ASSET);
+  // }
+  // else if (env[2]) {
+  //   ASSET = env[2] +  env.prod ? 'prod' : 'preprod' + '/';
+  //   console.log(ASSET);
+  // }
+  // else if (configuration.application.cdn && configuration.application.directory) {
+  //   ASSET =  configuration.application.cdn + configuration.application.directory;
+  //   if (!ONPREM ) {
+  //     ASSET += env.prod ? 'prod/' : 'preprod/';
+  //   }
+  // }
+
+  return Merge.strategy({ plugins: 'prepend' })(common(env), {
     devtool: 'source-map',
     mode: 'production',
     output: {
       filename: 'bundle.min.js',
       chunkLoadingGlobal: 'dydu.bliss',
-      chunkFilename: `[name].${PACKAGE.version || 'lts'}.js`,
+      chunkFilename: `[name].${env.CHATBOX_VERSION || 'lts.xxxxxx'}.js`,
       path: Path.resolve(__dirname, 'build'),
-      publicPath:  ASSET
+      publicPath: ASSET,
     },
     plugins: [
       new Clean(),
       new Copy({
         patterns: [
-              {
-                from: "**/*",
-                context: Path.resolve(__dirname, "public"),
-                globOptions: {
-                  dot: true,
-                  gitignore: true,
-                  ignore: ["**/index.html", "**/*.json.sample", "**/*.css.sample"],
-                },
-              },
-        ]
+          {
+            from: '**/*',
+            context: Path.resolve(__dirname, 'public'),
+            globOptions: {
+              dot: true,
+              gitignore: true,
+              ignore: ['**/index.html', '**/*.json.sample', '**/*.css.sample'],
+            },
+          },
+        ],
       }),
       new webpack.DefinePlugin({
         'process.env': {
           OIDC_CLIENT_ID: OIDC_CLIENT_ID,
           OIDC_URL: OIDC_URL,
           PUBLIC_URL: JSON.stringify(ASSET),
-          QUALIFICATION
-        }
-      })
+          QUALIFICATION,
+        },
+      }),
     ],
     stats: 'verbose',
   });
