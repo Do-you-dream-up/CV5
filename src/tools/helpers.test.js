1b03076511631455fa2d6073878e19463500e09d
Guillaume <gloup@dydu.ai>
2022-12-19 17:52:32 +0100

    add tests for helpers

--- src/tools/helpers.test.js
+++ src/tools/helpers.test.js
@@ -1,5 +1,24 @@
-import { lowercaseKeys, b64decode, b64dFields, isDefined, isEmptyArray, isOfType, toFormUrlEncoded } from './helpers';
+import {
+  b64dFields,
+  b64decode,
+  b64encode,
+  isDefined,
+  isEmptyArray,
+  isEmptyObject,
+  isEmptyString,
+  isObject,
+  isOfType,
+  isOfTypeBoolean,
+  isOfTypeFunction,
+  isOfTypeNumber,
+  isOfTypeString,
+  isPositiveNumber,
+  strContains,
+  toFormUrlEncoded,
+} from './helpers';
+
 import { VAR_TYPE } from './constants';
+import { expect } from '@jest/globals';
 
 describe('helpers', () => {
   describe('isDefined', () => {
@@ -98,6 +117,239 @@ describe('helpers', () => {
       const param = btoa('decoded');
       expect(typeof param).toEqual('string');
       expect(b64decode(param)).toEqual('decoded');
+    });
+  });
+
+  describe('b64encode', () => {
+    it('should encode when param is of type string', () => {
+      //GIVEN
+      const target = 'bonjour';
+      const expected = 'Ym9uam91cg==';
+
+      //WHEN
+      const result = b64encode(target);
+
+      //THEN
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('isOfTypeString', () => {
+    it('should return true when parameter is of type string', () => {
+      //GIVEN
+      const target = 'bonjour';
+
+      //WHEN
+      const result = isOfTypeString(target);
+
+      //THEN
+      expect(result).toEqual(true);
+    });
+
+    it('should return false when parameter is NOT type of string', () => {
+      //GIVEN
+      const target = 5;
+
+      //WHEN
+      const result = isOfTypeString(target);
+
+      //THEN
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('isEmptyString', () => {
+    it('should return true when parameter is of type string and is empty', () => {
+      //GIVEN
+      const target = '';
+
+      //WHEN
+      const result = isEmptyString(target);
+
+      //THEN
+      expect(result).toEqual(true);
+    });
+
+    it('should return false when parameter is of type string and is NOT empty', () => {
+      //GIVEN
+      const target = 'bonjour';
+
+      //WHEN
+      const result = isEmptyString(target);
+
+      //THEN
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('isEmptyObject', () => {
+    it('should return true when param is type of object and is empty', () => {
+      //GIVEN
+      const target = {};
+
+      //WHEN
+      const result = isEmptyObject(target);
+
+      //THEN
+      expect(result).toEqual(true);
+    });
+
+    it('should return false when param is type of object and is NOT empty', () => {
+      //GIVEN
+      const target = { dog: 'jack russel', name: 'Havane' };
+
+      //WHEN
+      const result = isEmptyObject(target);
+
+      //THEN
+      expect(result).toEqual(false);
+    });
+  });
+  isEmptyObject;
+
+  describe('isOfTypeBoolean', () => {
+    it('should return true when param is type of boolean', () => {
+      //GIVEN
+      const target = false;
+
+      //WHEN
+      const result = isOfTypeBoolean(target);
+
+      //THEN
+      expect(result).toEqual(true);
+    });
+
+    it('should return false when param is NOT type of boolean', () => {
+      //GIVEN
+      const target = 'false';
+
+      //WHEN
+      const result = isOfTypeBoolean(target);
+
+      //THEN
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('isObject', () => {
+    it('should return true when param is type of object', () => {
+      //GIVEN
+      const target = { dog: 'jack russel', name: 'Havane' };
+
+      //WHEN
+      const result = isObject(target);
+
+      //THEN
+      expect(result).toEqual(true);
+    });
+
+    it('should return false when param is NOT type of object', () => {
+      //GIVEN
+      const target = 'bonjour';
+
+      //WHEN
+      const result = isObject(target);
+
+      //THEN
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('isOfTypeFunction', () => {
+    it('should return true when param is type of function', () => {
+      //GIVEN
+      const target = () => {};
+
+      //WHEN
+      const result = isOfTypeFunction(target);
+
+      //THEN
+      expect(result).toEqual(true);
+    });
+
+    it('should return false when param is NOT type of function', () => {
+      //GIVEN
+      const target = 'bonjour';
+
+      //WHEN
+      const result = isOfTypeFunction(target);
+
+      //THEN
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('isOfTypeNumber', () => {
+    it('should return true when param is type of number', () => {
+      //GIVEN
+      const target = 8;
+
+      //WHEN
+      const result = isOfTypeNumber(target);
+
+      //THEN
+      expect(result).toEqual(true);
+    });
+
+    it('should return false when param is NOT type of number', () => {
+      //GIVEN
+      const target = '8';
+
+      //WHEN
+      const result = isOfTypeNumber(target);
+
+      //THEN
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('isPositiveNumber', () => {
+    it('should return true when param is a number greater than 0', () => {
+      //GIVEN
+      const target = 8;
+
+      //WHEN
+      const result = isPositiveNumber(target);
+
+      //THEN
+      expect(result).toEqual(true);
+    });
+
+    it('should return false when param is a number NOT greater than 0', () => {
+      //GIVEN
+      const target = 0;
+
+      //WHEN
+      const result = isPositiveNumber(target);
+
+      //THEN
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('strContains', () => {
+    it('should return true when param string contains the substring param', () => {
+      //GIVEN
+      const str = 'reactisverygood';
+      const substr = 'isveryg';
+
+      //WHEN
+      const result = strContains(str, substr);
+
+      //THEN
+      expect(result).toEqual(true);
+    });
+
+    it('should return false when param string NOT contains the substring param', () => {
+      //GIVEN
+      const str = 'reactisverygood';
+      const substr = 'chamallow';
+
+      //WHEN
+      const result = strContains(str, substr);
+
+      //THEN
+      expect(result).toEqual(false);
     });
   });
 });
