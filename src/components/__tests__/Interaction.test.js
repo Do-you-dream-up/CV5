53bd8f2999f745f7b524dbce6a6da98b3fee461c
Maxime Gauché-Daumet <maxime.gd@gmail.com>
2023-01-11 16:00:00 +0100

    fix interaction test

---
+++ src/components/__tests__/Interaction.test.js
@@ -0,0 +1,27 @@
+import { ContextQuery, contextName } from '../../setupTests';
+
+import Interaction from '../Interaction/index';
+import { render } from '../../tools/test-utils';
+
+jest.mock('../../contexts/DialogContext', () => ({
+  useDialog: jest.fn().mockReturnValue({ startAnimationOperatorWriting: false }),
+}));
+
+describe('Interaction', function () {
+  describe('Writing', function () {
+    it('should show loader', function () {
+      const { debug } = render(<Interaction.Writing />);
+      debug();
+    });
+
+    it('should show loader', function () {
+      const contextList = [
+        ContextQuery.make(contextName.configurationContext),
+        ContextQuery.make(contextName.dialogContext, { startAnimationOperatorWriting: false }),
+      ];
+
+      const { debug } = render(<Interaction.Writing />, { contexts: contextList });
+      debug();
+    });
+  });
+});
