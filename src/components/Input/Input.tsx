65c89faa498ed410c21eb09c5d3c392681b626c9
Guillaume <gloup@dydu.ai>
2023-02-09 15:38:23 +0100

    updtae rgaa counter character remaining

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -97,15 +97,10 @@ export default function Input({ onRequest, onResponse }: InputProps) {
           />
           <div children={input} className={classes.fieldShadow} />
           {!!showCounter && (
-            <>
-              <span
-                id="characters-remaining"
-                children={counter}
-                className={classes.counter}
-                placeholder={`${counter} ${counterRemaining}`}
-              />
+            <div id="characters-remaining">
+              <span children={counter} className={classes.counter} placeholder={`${counter} ${counterRemaining}`} />
               <span className={c('dydu-counter-hidden', classes.hidden)}>{`${counterRemaining}`}</span>
-            </>
+            </div>
           )}
         </div>
       );
