7786dff89832889e37bdb73f12beb2700e6daaa1
Jianni Nirinarisoa <jnirinarisoa@dydu.ai>
2023-03-07 11:27:35 +0100

    test(main): #sub remove JSDOM from setupTest

--- src/setupTests.js
+++ src/setupTests.js
@@ -12,12 +12,6 @@ const config = {
   domain: 'example.com',
 };
 
-const dom = new JSDOM('', config);
-global.document = dom.window.document;
-global.document.domain = config.domain;
-global.window = dom.window;
-global.location = dom.window.location;
-
 export const contextName = {
   dialogContext: 'dialogContext',
   configurationContext: 'configurationContext',
