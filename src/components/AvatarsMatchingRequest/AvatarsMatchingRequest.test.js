b14dd76a615ea9116d671a17dfe0917f00d0e4fa
Guillaume <gloup@dydu.ai>
2023-03-08 11:07:15 +0100

    add tests

--- src/components/AvatarsMatchingRequest/AvatarsMatchingRequest.test.js
+++ src/components/AvatarsMatchingRequest/AvatarsMatchingRequest.test.js
@@ -1,7 +1,15 @@
+import { render, screen } from '../../tools/test-utils';
+
 import AvatarsMatchingRequest from './AvatarsMatchingRequest';
-import { render } from '@testing-library/react';
 
 describe('AvatarsMatchingRequest', () => {
+  let mockConfiguration = {
+    header: {
+      logo: {
+        image: true,
+      },
+    },
+  };
   it('should render nothing if neither showHeaderAvatar or showInteractionAvatar is true', () => {
     const { container } = render(<AvatarsMatchingRequest type="" />);
     expect(container.firstChild).toBeNull();
@@ -10,5 +18,17 @@ describe('AvatarsMatchingRequest', () => {
   it('should render default avatar if no customAvatar and no typeResponse provided', () => {
     const { container } = render(<AvatarsMatchingRequest type="" defaultAvatar="default-avatar.png" />);
     expect(container.firstChild).toBeNull();
+  });
+
+  it('should render data test id avatar when img is return', () => {
+    const headerAvatar = true;
+
+    render(<AvatarsMatchingRequest headerAvatar={headerAvatar} />, {
+      configuration: mockConfiguration,
+    });
+
+    const element = screen.getByAltText('avatar');
+
+    expect(element).toBeTruthy();
   });
 });
