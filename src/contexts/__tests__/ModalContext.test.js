65c0513aa3a1db3555d90cad1a1aef2febba6c29
Maxime Gauché-Daumet <maxime.gd@gmail.com>
2023-03-14 14:12:21 +0100

    ModalContext

--- src/contexts/__tests__/ModalContext.test.js
+++ src/contexts/__tests__/ModalContext.test.js
@@ -6,6 +6,26 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { render } from '../../tools/test-utils';
 
+const renderComponent = (context, actionParam = null, optionsParam = null) => {
+  const { Component, modal, onReject, onResolve, options, thinking } = context;
+  return (
+    <>
+      <p>Component: {Component ? 'open' : 'closed'}</p>
+      <p>Thinking: {thinking ? 'true' : 'false'}</p>
+      <button data-testid="modal-button-open" onClick={() => modal(<div>Test Modal</div>, actionParam, optionsParam)}>
+        Open Modal
+      </button>
+      <button data-testid="modal-button-resolve" onClick={onResolve}>
+        Resolve Modal
+      </button>
+      <button data-testid="modal-button-reject" onClick={onReject}>
+        Reject Modal
+      </button>
+      <p>Options: {JSON.stringify(options)}</p>
+    </>
+  );
+};
+
 describe('ModalProvider', () => {
   it('renders children without crashing', () => {
     const { container } = render(
@@ -18,23 +38,8 @@ describe('ModalProvider', () => {
 
   it('provides modal context values to children', () => {
     const ChildComponent = () => {
-      const { Component, modal, onReject, onResolve, options, thinking } = React.useContext(ModalContext);
-      return (
-        <>
-          <p>Component: {Component ? 'open' : 'closed'}</p>
-          <p>Thinking: {thinking ? 'true' : 'false'}</p>
-          <button data-testid="modal-button-open" onClick={() => modal(<div>Test Modal</div>)}>
-            Open Modal
-          </button>
-          <button data-testid="modal-button-resolve" onClick={onResolve}>
-            Resolve Modal
-          </button>
-          <button data-testid="modal-button-reject" onClick={onReject}>
-            Reject Modal
-          </button>
-          <p>Options: {JSON.stringify(options)}</p>
-        </>
-      );
+      const context = React.useContext(ModalContext);
+      return renderComponent(context);
     };
 
     ChildComponent.propTypes = {
@@ -68,5 +73,34 @@ describe('ModalProvider', () => {
 
     getByTestId('modal-button-open').click();
     expect(getByText('Component: open')).toBeInTheDocument();
+  });
+
+  it('provides modal context values to children and action on resolve', () => {
+    const functionForResolve = () => console.log('resolve');
+
+    const ChildComponent = () => {
+      const context = React.useContext(ModalContext);
+      return renderComponent(context, functionForResolve);
+    };
+
+    ChildComponent.propTypes = {
+      onButtonClick: PropTypes.func,
+    };
+
+    const { getByText, getByTestId } = render(
+      <ModalProvider>
+        <ChildComponent />
+      </ModalProvider>,
+    );
+
+    expect(getByText('Component: closed')).toBeInTheDocument();
+    expect(getByText('Thinking: false')).toBeInTheDocument();
+    expect(getByTestId('modal-button-open')).toBeInTheDocument();
+    expect(getByTestId('modal-button-resolve')).toBeInTheDocument();
+    expect(getByTestId('modal-button-reject')).toBeInTheDocument();
+    expect(getByText('Options: {"dismissable":true,"variant":"center"}')).toBeInTheDocument();
+
+    getByTestId('modal-button-resolve').click();
+    expect(getByText('Component: closed')).toBeInTheDocument();
   });
 });
