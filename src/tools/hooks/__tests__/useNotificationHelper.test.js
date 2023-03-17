a9d6729417c67fa8c8f4bd6dc5d645da3b51812f
Maxime Gauché-Daumet <maxime.gd@gmail.com>
2023-03-17 10:20:25 +0100

    add base test for useNotificationHelper

---
+++ src/tools/hooks/__tests__/useNotificationHelper.test.js
@@ -0,0 +1,11 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useNotificationHelper from '../useNotificationHelper';
+
+describe('useNotificationHelper', () => {
+  test('should return null values for undefined notification', () => {
+    const { result } = renderHook(() => useNotificationHelper(undefined));
+    expect(result.current.text).toBe(undefined);
+    expect(result.current.iconName).toBeNull();
+    expect(result.current.isWriting).toBeNull();
+  });
+});
