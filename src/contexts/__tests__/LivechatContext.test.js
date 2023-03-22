0ae73718212ef1f393b13e60189d0dbc8044928b
Maxime Gauché-Daumet <maxime.gd@gmail.com>
2023-03-22 11:17:18 +0100

    fix

--- src/contexts/__tests__/LivechatContext.test.js
+++ src/contexts/__tests__/LivechatContext.test.js
@@ -1,146 +1,92 @@
-import '@testing-library/jest-dom';
+import { LivechatProvider, useLivechat } from '../LivechatContext';
+import { render, screen } from '@testing-library/react';
 
-import { LivechatContext, LivechatProvider, useLivechat } from '../LivechatContext';
-import { act, render, screen } from '@testing-library/react';
-
-import { TUNNEL_MODE } from '../../tools/constants';
-import { renderHook } from '@testing-library/react-hooks';
-import { useCallback } from 'react';
+import React from 'react';
+import { useDialog } from '../DialogContext';
+import useDyduPolling from '../../tools/hooks/useDyduPolling';
 import useDyduWebsocket from '../../tools/hooks/useDyduWebsocket';
+import { useEvent } from '../EventsContext';
+import useQueue from '../../tools/hooks/useQueue';
+import { useSurvey } from '../../Survey/SurveyProvider';
+
+jest.mock('../../Survey/SurveyProvider');
+jest.mock('../DialogContext');
+jest.mock('../EventsContext');
+jest.mock('../../tools/hooks/useQueue');
+jest.mock('../../tools/hooks/useDyduPolling');
+jest.mock('../../tools/hooks/useDyduWebsocket');
 
 describe('LivechatProvider', () => {
   beforeEach(() => {
-    // Clear any mocks and reset any state between tests
+    useSurvey.mockReturnValue({
+      showSurvey: jest.fn(),
+    });
+    useDialog.mockReturnValue({
+      lastResponse: {},
+      displayNotification: jest.fn(),
+      showAnimationOperatorWriting: jest.fn(),
+    });
+    useEvent.mockReturnValue({
+      onNewMessage: jest.fn(),
+    });
+    useQueue.mockReturnValue({
+      pop: jest.fn(),
+      put: jest.fn(),
+      list: [],
+      isEmpty: true,
+    });
+    useDyduPolling.mockReturnValue({
+      mode: 'polling',
+      isAvailable: jest.fn(),
+      open: jest.fn(),
+      send: jest.fn(),
+      onUserTyping: jest.fn(),
+    });
+    useDyduWebsocket.mockReturnValue({
+      mode: 'websocket',
+      isAvailable: jest.fn(),
+      open: jest.fn(),
+      send: jest.fn(),
+      onUserTyping: jest.fn(),
+    });
+  });
+
+  afterEach(() => {
     jest.clearAllMocks();
-    jest.resetAllMocks();
-    jest.restoreAllMocks();
   });
 
   it('should render children', () => {
-    // Arrange
     const ChildComponent = () => <div>Child Component</div>;
-    const props = { children: <ChildComponent /> };
-
-    // Act
-    render(<LivechatProvider {...props} />);
-    const childComponent = screen.getByText(/Child Component/);
-
-    // Assert
-    expect(childComponent).toBeInTheDocument();
-  });
-
-  it('should call onNewMessage and window.dydu.chat.reply', () => {
-    const onNewMessageMock = jest.fn();
-    window.dydu = { chat: { reply: jest.fn() } };
-
-    const { result } = renderHook(() => useLivechat(onNewMessageMock), { wrapper: LivechatProvider });
-
-    act(() => {
-      console.log('🚀 ~ file: LivechatContext.test.js:37 ~ act ~ result.current:', result.current);
-      result.current.displayResponseText('Hello World');
-    });
-
-    expect(window.dydu.chat.reply).toHaveBeenCalledWith('Hello World');
-  });
-
-  it('should open a WebSocket tunnel by default', async () => {
-    const { result } = renderHook(
-      () =>
-        useDyduWebsocket({
-          mode: TUNNEL_MODE.websocket,
-          open: jest.fn().mockResolvedValue(undefined),
-          isAvailable: () => true,
-        }),
-      { wrapper: LivechatProvider },
+    render(
+      <LivechatProvider>
+        <ChildComponent />
+      </LivechatProvider>,
     );
-
-    await act(async () => {
-      render(
-        <LivechatProvider>
-          <LivechatContext>{({ isWebsocket }) => <div>{isWebsocket.toString()}</div>}</LivechatContext>
-        </LivechatProvider>,
-      );
-    });
-
-    expect(screen.getByText('false')).toBeDefined();
+    expect(screen.getByText('Child Component')).toBeDefined();
   });
 
-  describe('typing', () => {
-    it('should call onUserTyping function with input', () => {
-      const onUserTyping = jest.fn();
-      const tunnel = {
-        onUserTyping,
-      };
-      const { result } = renderHook(() =>
-        useCallback(
-          (input) => {
-            tunnel?.onUserTyping(input);
-          },
-          [tunnel],
-        ),
+  it('should provide livechat context', () => {
+    const ChildComponent = () => {
+      const { isWebsocket, send, isLivechatOn, typing, sendSurvey } = useLivechat();
+      return (
+        <div>
+          <span>{isWebsocket.toString()}</span>
+          <span>{isLivechatOn.toString()}</span>
+          <button onClick={() => send('test')}>Send</button>
+          <button onClick={() => typing('test')}>Typing</button>
+          <button onClick={() => sendSurvey('test')}>Send Survey</button>
+        </div>
       );
-
-      result.current('hello');
-
-      expect(onUserTyping).toHaveBeenCalledWith('hello');
-    });
-
-    it('should not call onUserTyping function if tunnel is not defined', () => {
-      const onUserTyping = jest.fn();
-      const tunnel = undefined;
-      const { result } = renderHook(() =>
-        useCallback(
-          (input) => {
-            tunnel?.onUserTyping(input);
-          },
-          [tunnel],
-        ),
-      );
-
-      result.current('hello');
-
-      expect(onUserTyping).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('sendSurvey', () => {
-    it('calls tunnel.sendSurvey if tunnel exists', () => {
-      // Mock the tunnel object
-      const mockTunnel = { sendSurvey: jest.fn() };
-
-      // Render the hook with the mock tunnel object
-      const { result } = renderHook(() => useLivechat(mockTunnel), { wrapper: LivechatProvider });
-
-      // Call the sendSurvey function with a survey response
-      result.current.sendSurvey({ question1: 'answer1', question2: 'answer2' });
-    });
-
-    it('calls put function if tunnel does not exist', () => {
-      // Mock the put function
-      const mockPut = jest.fn();
-
-      // Render the hook without a tunnel object
-      const { result } = renderHook(() => useLivechat(null, mockPut), { wrapper: LivechatProvider });
-
-      // Call the sendSurvey function with a survey response
-      result.current.sendSurvey({ question1: 'answer1', question2: 'answer2' });
-    });
-
-    it('logs a message to the console', () => {
-      // Mock the console.log function
-      console.log = jest.fn();
-
-      // Render the hook without a tunnel object or put function
-      const { result } = renderHook(() => useLivechat(), { wrapper: LivechatProvider });
-
-      // Call the sendSurvey function with a survey response
-      result.current.sendSurvey({ question1: 'answer1', question2: 'answer2' });
-
-      // Check if console.log was called with the correct message
-      expect(console.log).toHaveBeenCalledWith('livechat sending survey', {
-        question1: 'answer1',
-        question2: 'answer2',
-      });
-    });
+    };
+    render(
+      <LivechatProvider>
+        <ChildComponent />
+      </LivechatProvider>,
+    );
+    screen.getByRole('button', { name: 'Send' }).click();
+    expect(useDyduPolling().send).toBeDefined();
+    screen.getByRole('button', { name: 'Typing' }).click();
+    screen.getByRole('button', { name: 'Send Survey' }).click();
+    expect(useQueue().put).toHaveBeenCalledWith('test');
   });
 });
