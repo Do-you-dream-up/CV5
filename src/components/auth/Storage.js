f762bd1698f07366a59ca97833ebd3590c31b64d
Maxime Gauché-Daumet <maxime.gd@gmail.com>
2023-03-23 11:38:29 +0100

    update storage to match values

--- src/components/auth/Storage.js
+++ src/components/auth/Storage.js
@@ -3,6 +3,7 @@ import { cleanUrl, isDefined } from './helpers';
 const store = localStorage;
 
 const PKCE_KEY = 'pkce';
+const TOKEN_KEY_ID = 'dydu-oauth-token-id';
 const TOKEN_KEY_ACCESS = 'dydu-oauth-token-access';
 const TOKEN_KEY_REFRESH = 'dydu-oauth-token-refresh';
 
@@ -23,18 +24,21 @@ export default class Storage {
   static saveToken = (token) => {
     cleanUrl();
     Storage.clearPkce();
+    localStorage.setItem(TOKEN_KEY_ID, token?.id_token);
     localStorage.setItem(TOKEN_KEY_ACCESS, token?.access_token);
     localStorage.setItem(TOKEN_KEY_REFRESH, token?.refresh_token);
   };
 
   static clearToken() {
+    localStorage.removeItem(TOKEN_KEY_ID);
     localStorage.removeItem(TOKEN_KEY_ACCESS);
     localStorage.removeItem(TOKEN_KEY_REFRESH);
   }
 
   static loadToken() {
     return (
-      isDefined(localStorage.getItem(TOKEN_KEY_ACCESS)) && {
+      isDefined(localStorage.getItem(TOKEN_KEY_ID)) && {
+        id_token: isDefined(localStorage.getItem(TOKEN_KEY_ID)) && localStorage.getItem(TOKEN_KEY_ID),
         access_token: isDefined(localStorage.getItem(TOKEN_KEY_ACCESS)) && localStorage.getItem(TOKEN_KEY_ACCESS),
         refresh_token: isDefined(localStorage.getItem(TOKEN_KEY_REFRESH)) && localStorage.getItem(TOKEN_KEY_REFRESH),
       }
