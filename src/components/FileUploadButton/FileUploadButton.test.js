415090118670e49633e4496ec742b6cad8dbc89d
Guillaume <gloup@dydu.ai>
2023-03-28 17:13:37 +0200

    start tests for fileuploadbutton

---
+++ src/components/FileUploadButton/FileUploadButton.test.js
@@ -0,0 +1,29 @@
+import '@testing-library/jest-dom';
+
+import * as helpers from '../../tools/helpers';
+
+import { act, fireEvent, waitFor } from '@testing-library/react';
+
+import FileUploadButton from './FileUploadButton';
+import { render } from '../../tools/test-utils';
+import { useUploadFile } from '../../contexts/UploadFileContext';
+
+jest.mock('../../contexts/UploadFileContext', () => ({
+  useUploadFile: jest.fn(),
+}));
+
+describe('FileUploadButton', () => {
+  it('should call the onSelectFile method on click', async () => {
+    const onSelectFileMock = jest.fn();
+
+    useUploadFile.mockReturnValue({
+      onSelectFile: onSelectFileMock,
+      extractFileFromEvent: { name: 'fileName' },
+    });
+
+    const { getByTestId } = render(<FileUploadButton />);
+    fireEvent.click(getByTestId('file-upload-button'));
+
+    expect(getByTestId('file-upload-button')).toBeInTheDocument();
+  });
+});
