6dd8eec8a37f5a4856ce9db278fe97dc968ae791
Guillaume <gloup@dydu.ai>
2023-03-29 17:25:19 +0200

    add tests for uploadinput component

--- src/components/UploadInput/UploadInput.test.js
+++ src/components/UploadInput/UploadInput.test.js
@@ -1,12 +1,69 @@
 import '@testing-library/jest-dom';
 
-import UploadInput from './UploadInput';
-import { render } from '../../tools/test-utils';
+import { act, fireEvent, render, waitFor } from '@testing-library/react';
 
+import UploadInput from './UploadInput';
+import dydu from '../../tools/dydu';
+import { useTranslation } from 'react-i18next';
+import { useUploadFile } from '../../contexts/UploadFileContext';
+
+jest.mock('../../tools/dydu', () => ({
+  __esModule: true,
+  default: {
+    sendUpoadFile: jest.fn(),
+  },
+}));
+
+jest.mock('react-i18next', () => ({
+  useTranslation: jest.fn().mockReturnValue({
+    t: jest.fn().mockImplementation((string) => {
+      if (string === 'input.actions.send') {
+        return 'Envoie';
+      } else {
+        return 'Remplacer le fichier à télécharger';
+      }
+    }),
+  }),
+}));
+jest.mock('../../contexts/UploadFileContext', () => ({
+  useUploadFile: jest.fn().mockReturnValue({
+    fileSelected: '',
+    handleCancel: () => {},
+    errorFormatMessage: '',
+    isUploadFileSent: false,
+    fileName: 'file name',
+  }),
+}));
 describe('UploadInput', () => {
   it('render the UploadInput component', () => {
-    const { getByTestId } = render(<UploadInput />);
+    let screen;
+    act(() => {
+      screen = render(<UploadInput />);
+    });
 
-    expect(getByTestId('footer-upload-input')).toBeInTheDocument();
+    expect(screen.getByTestId('footer-upload-input')).toBeInTheDocument();
+  });
+
+  test('should call sendUpoadFile function when Send button is clicked', () => {
+    const mockSendUploadFile = jest.spyOn(dydu, 'sendUpoadFile');
+    const fileContent = 'file content';
+    const file = new File([new Blob([fileContent], { type: 'application/pdf' })], 'filename.pdf', {
+      type: 'application/pdf',
+    });
+
+    const isUploadFileSentMock = jest.fn();
+    let screen;
+    act(() => {
+      screen = render(<UploadInput />);
+    });
+
+    fireEvent.change(screen.getByText('Remplacer le fichier à télécharger'), {
+      target: { files: [file] },
+    });
+    fireEvent.click(screen.getByText('Envoie'));
+
+    waitFor(() => expect(isUploadFileSentMock).toHaveBeenCalled());
+
+    waitFor(() => expect(mockSendUploadFile).toHaveBeenCalledWith(file));
   });
 });
