25597cca224998cf52008f61b62cb39ec0791cd3
Maxime Gauché-Daumet <maxime.gd@gmail.com>
2023-03-29 21:41:47 +0200

    test build remove data testid

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,16 @@
 module.exports = (api) => {
   api.cache(true);
+
+  const plugins = ['@babel/plugin-syntax-dynamic-import', '@babel/plugin-proposal-class-properties'];
+
+  if (process.env["NODE_ENV"] === "production") {
+    plugins.push(['remove-object-properties', { regexp: 'data-test*' }])
+  }
+  console.log("🚀 ~ file: babel.config.js:7 ~ process.env", process.env["ENV"])
+  console.log("🚀 ~ file: babel.config.js:8 ~ plugins:", plugins)
+
   return {
-    env: {
-      production: {
-        plugins: ['react-remove-properties', {"properties": ["data-testid"]}],
-      },
-    },
-    plugins: ['@babel/plugin-syntax-dynamic-import', '@babel/plugin-proposal-class-properties'],
+    plugins,
     presets: [
       [
         '@babel/preset-env',
