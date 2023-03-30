62e45da984af8a2ef959e8dc70fccde8c18954f7
Maxime Gauché-Daumet <mgauchedaumet@dydu.ai>
2023-03-30 08:29:36 +0000

    Merge branch 'fix_test-build-fail' into 'main'
    Fix test build fail
    
    See merge request front/bliss!611

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = (api) => {
   api.cache(true);
+
+  const plugins = ['@babel/plugin-syntax-dynamic-import', '@babel/plugin-proposal-class-properties'];
+
+  if (process.env.npm_lifecycle_event === 'build' || process.env.WEBPACK_SERVE === 'true') {
+    /** Add plugin packages for build process */
+    plugins.push(['remove-object-properties', { regexp: 'data-test*' }])
+  }
+
   return {
     plugins: ['@babel/plugin-syntax-dynamic-import', '@babel/plugin-proposal-class-properties'],
     presets: [
