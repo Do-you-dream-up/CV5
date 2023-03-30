7f6cf1b930f0ad5b021aab9af77aebcd75006dc3
Maxime Gauché-Daumet <maxime.gd@gmail.com>
2023-03-30 08:17:20 +0200

    remove logs

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = (api) => {
 
   const plugins = ['@babel/plugin-syntax-dynamic-import', '@babel/plugin-proposal-class-properties'];
 
-  console.log("🚀 ~ file: babel.config.js:7 ~ process.env:", process.env.WEBPACK_SERVE, process.env.WEBPACK_SERVE === 'true')
   if (process.env.npm_lifecycle_event === 'build' || process.env.WEBPACK_SERVE === 'true') {
     /** Add plugin packages for build process */
     plugins.push(['remove-object-properties', { regexp: 'data-test*' }])
