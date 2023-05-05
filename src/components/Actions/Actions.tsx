1ca0e0591f5d7ee6ce671b98a2219a71dadcb741
MarcHazime <marc.hazime@gmail.com>
2023-05-05 16:09:25 +0200

    add unique key to filteredaction map

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -52,11 +52,11 @@ const Actions = ({ actions = [], className, targetStyleKey, testId, role }: Acti
 
   return actions?.length > 0 ? (
     <div className={c('dydu-actions', _classes, className)} aria-labelledby={testId} role={role}>
-      {filteredActions.map(({ items, selected, type = 'button', title, testId, ...rest }, index) => {
+      {filteredActions.map(({ items, selected, type = 'button', title, testId, ...rest }) => {
         delete rest.when;
 
         const props: ButtonProps = {
-          key: index,
+          key: title,
           ...rest,
           title,
           type,
