609bed3217e5cca497a7096f25648efd7b739c4f
mhazime <mhazime@dydu.ai>
2023-07-13 16:38:30 +0200

    fix test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,25 +2,10 @@ import '@testing-library/jest-dom';
 
 import App from './App';
 import { render } from '../../tools/test-utils';
-import { useTranslation } from 'react-i18next';
-
-jest.mock('react-i18next', () => ({
-  useTranslation: jest.fn(),
-}));
 
 describe('App', () => {
   beforeEach(() => {
     jest.clearAllMocks();
-
-    const tSpy = jest.fn((str) => str);
-    const useTranslationSpy = useTranslation;
-
-    useTranslationSpy.mockReturnValue({
-      t: tSpy,
-      i18n: {
-        language: 'en',
-      },
-    });
   });
 
   test('Should render App component', async () => {
