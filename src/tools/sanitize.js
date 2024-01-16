b50364e4d7c6698e5cb3bc8d922edc2b9832a0f5
csarraud <csarraud@dydu.ai>
2024-01-16 12:27:17 +0100

    fix(main): #RDS-4068 erreur js dans la cv5 aboutissant a la fermeture totale de la boite de dialogue

--- src/tools/sanitize.js
+++ src/tools/sanitize.js
@@ -1,11 +1,65 @@
+import sanitizeHtml from 'sanitize-html';
+
 /**
  * Sanitize the provided HTML string.
+ *
+ * authorizedHtmlItems contains all basics html items from mdn doc that aren't already in the default config of sanitized html lib
  *
  * @param {string} html - HTML string to sanitize.
  * @returns {string} The sanitized string.
  */
 export default (html) => {
-  const element = document.createElement('div');
-  element.innerHTML = html || '';
-  return element.innerHTML;
+  return sanitizeHtml(html, {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(authorizedHtmlItems),
+    allowedAttributes: false,
+  });
 };
+
+const authorizedHtmlItems = [
+  'img',
+  'area',
+  'audio',
+  'map',
+  'track',
+  'video',
+  'embed',
+  'iframe',
+  'object',
+  'picture',
+  'portal',
+  'source',
+  'svg',
+  'math',
+  'canvas',
+  'noscript',
+  'script',
+  'del',
+  'ins',
+  'button',
+  'datalist',
+  'fieldset',
+  'form',
+  'input',
+  'label',
+  'legend',
+  'meter',
+  'optgroup',
+  'option',
+  'output',
+  'progress',
+  'select',
+  'textarea',
+  'details',
+  'dialog',
+  'summary',
+  'slot',
+  'template',
+  'body',
+  'html',
+  'base',
+  'head',
+  'link',
+  'meta',
+  'style',
+  'title',
+];
