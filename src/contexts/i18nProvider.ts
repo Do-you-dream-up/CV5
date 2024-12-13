b0fae8d731d3e01ec7bc0993f06254a58f865938
DominiqueF <dfoueillassar@dydu.ai>
2024-12-13 17:30:56 +0100

    fix(main): #RDS-4806 revert back i18next-http-backend to i18next-xhr-backend, do not follow anymore compilation warnings!!!

--- src/contexts/i18nProvider.ts
+++ src/contexts/i18nProvider.ts
@@ -1,6 +1,6 @@
 import { Cookie, Local } from '../tools/storage';
 
-import Backend from 'i18next-http-backend';
+import Backend from 'i18next-xhr-backend';
 import BrowserLanguage from 'i18next-browser-languagedetector';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
